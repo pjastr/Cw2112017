8d713b08304adb35640ab0993f15d93570ef67b6
pjastr <24464854+pjastr@users.noreply.github.com>
2017-11-02 18:18:08 +0100

    Add files via upload

---
+++ ConsoleApplication40/ConsoleApplication40/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication40
+{
+    class Car:IComparable<Car>
+    {
+        private int year;
+        private string brand;
+
+        public Car(int year, string brand)
+        {
+            this.brand = brand;
+            this.year = year;
+        }
+
+        public string Info()
+        {
+            return brand + " " + year;
+        }
+
+        public int CompareTo(Car other)
+        {
+            if (this.year == other.year)
+            {
+                return other.brand.CompareTo(this.brand);
+            }
+            else
+            {
+                return other.year.CompareTo(this.year);
+            }
+        }
+    }
+}
